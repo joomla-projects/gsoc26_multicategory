db0cd5f44bdcff0114ecf98763bb7036cbba5afa
Reda <reda.mohamed.reda.201@gmail.com>
2026-06-12 02:30:49 +0300

    fix php style

--- components/com_content/src/Model/ArticlesModel.php
+++ components/com_content/src/Model/ArticlesModel.php
@@ -768,7 +768,7 @@ class ArticlesModel extends ListModel
             ->where($db->quoteName('c.published') . ' = 1')
             ->bind(':context', $context);
 
-            // Filter by language
+        // Filter by language
         if ($this->getState('filter.language')) {
             $query->whereIn($db->quoteName('c.language'), [Factory::getApplication()->getLanguage()->getTag(), '*'], ParameterType::STRING);
         }
