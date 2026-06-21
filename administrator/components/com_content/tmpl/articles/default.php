de4950f649e4311ef7eebee866a9daea30291d53
Reda <reda.mohamed.reda.201@gmail.com>
2026-06-21 23:13:36 +0300

    Fix PHP CS

--- administrator/components/com_content/tmpl/articles/default.php
+++ administrator/components/com_content/tmpl/articles/default.php
@@ -30,8 +30,10 @@ use Joomla\Utilities\ArrayHelper;
 /** @var \Joomla\CMS\WebAsset\WebAssetManager $wa */
 $wa = $this->getDocument()->getWebAssetManager();
 $wa->useScript('table.columns')
-    ->useScript('multiselect')
-    ->addInlineStyle('#adminForm .js-stools-container-filters-visible { grid-template-columns: repeat(auto-fill, minmax(17rem, 1fr)); }'
+    ->useScript('multiselect');
+
+$wa->addInlineStyle(
+    '#adminForm .js-stools-container-filters-visible { grid-template-columns: repeat(auto-fill, minmax(17rem, 1fr)); }'
 );
 
 $app       = Factory::getApplication();
