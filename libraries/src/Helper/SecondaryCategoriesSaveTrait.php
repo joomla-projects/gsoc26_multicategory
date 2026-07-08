e26e2e623b12943098e64652c159237d2a1181fc
Reda <reda.mohamed.reda.201@gmail.com>
2026-07-09 01:26:27 +0300

    Refactor com contact

--- libraries/src/Helper/SecondaryCategoriesSaveTrait.php
+++ libraries/src/Helper/SecondaryCategoriesSaveTrait.php
@@ -194,11 +194,7 @@ trait SecondaryCategoriesSaveTrait
      */
     private function saveSecondaryCategories(array $data): void
     {
-        $itemId = (int) ($data['id'] ?? 0);
-
-        if (!$itemId) {
-            $itemId = (int) $this->getState($this->getName() . '.id');
-        }
+        $itemId = (int) $this->getState($this->getName() . '.id');
 
         $submitted = $data['secondary_categories'] ?? [];
 
