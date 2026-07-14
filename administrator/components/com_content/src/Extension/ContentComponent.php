77b7895855133de543d0d099295304e952b48958
Reda <reda.mohamed.reda.201@gmail.com>
2026-07-14 14:03:35 +0300

    Fix(com_contacts): Fix count with contacts in category list

--- administrator/components/com_content/src/Extension/ContentComponent.php
+++ administrator/components/com_content/src/Extension/ContentComponent.php
@@ -341,7 +341,7 @@ class ContentComponent extends MVCComponent implements
         ];
 
         LibraryContentHelper::countRelations($items, $config);
-
+        
         $this->countSecondaryCategoryItems($items);
     }
 
@@ -354,7 +354,7 @@ class ContentComponent extends MVCComponent implements
      *
      * @since   __DEPLOY_VERSION__
      */
-    private function countSecondaryCategoryItems(array $items): void
+    private function countSecondaryCategoryItems(&$items): void
     {
         if (empty($items)) {
             return;
